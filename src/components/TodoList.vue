c6aa9f783381c7c44b92195203bc846e2254dea3
artofnext <novnovalex@gmail.com>
2020-01-18 10:39:29 +0200

    add vue-draggable component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-drag-and-drop:options="options">
         <ul>
             <li v-for="(value, index) in todoArray" :key="index">
                 <p>{{ index + 1 }} - {{ value }}<button v-on:click="deleteItem(index)">x</button></p>
@@ -10,12 +10,21 @@
 </template>
 
 <script>
+// import VueDraggable from 'vue-draggable'
+
+import { VueDraggableDirective } from 'vue-draggable'
+
 export default {
     name: "TodoList",
     props: {
         todoArray: Array,
     },
-
+    directives: {
+        dragAndDrop: VueDraggableDirective
+    },
+    // components: {
+    //     VueDraggable,
+    // },
     methods: {
         deleteItem (ind) {
             // this.todoArray.splice(ind, 1);
